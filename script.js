da961dd447b3700ce72bc00b56cc2183099ececd
Dimitri Law <dimitrilaw112@gmail.com>
2022-10-18 11:56:18 -0400

    cors header

--- script.js
+++ script.js
@@ -17,23 +17,32 @@ for (i = 0; i < coll.length; i++) {
 // Logic for POST request to expensify API to return access token
  async function submitHandler (){
 var postURL = 'https://cors-anywhere.herokuapp.com/https://www.expensify.com/api?command=Authenticate'
-const partnerPassword = 'd7c3119c6cdab02d68d9'
-fetch(postURL,{
-    method: 'POST',
-    body: JSON.stringify({
-        partnerName: 'applicant',
-        partnerUserID: 'expensifytest@mailinator.com',
-        partnerPassword:partnerPassword,
-        partnerUserSecret: 'hire_me',
+const postUrl1 = 'https://cors-anywhere.herokuapp.com/https://www.expensify.com/api?command=Authenticate&partnerName=applicant&partnerPassword=d7c3119c6cdab02d68d9&partnerUserID=expensifytest@mailinator.com&partnerUserSecret=hire_me';
+//const partnerPassword = 'd7c3119c6cdab02d68d9';
+try{
+let response = await fetch(postUrl1,{
+   method: 'POST',
+//     body: JSON.stringify({
+//         partnerName: 'applicant',
+//         partnerUserID: 'expensifytest@mailinator.com',
+//         partnerPassword:'d7c3119c6cdab02d68d9',
+//         partnerUserSecret: 'hire_me',
 
-}),
-headers: { 'Content-type': 'application/json; charset=UTF-8' }
-}).then(res => {
+// }),
+ headers: { 'Access-Control-Allow-Origin': '*', 'Content-type': 'application/json; charset=UTF-8' }
+// }).then(res => {
    
-    return res.json()
-}).then(data =>{
-    console.log(data)
-});
+//     return res.json()
+// }).then(data =>{
+//     console.log(data)
+// });
 
+}
+ );
+ let data = await response.json();
+ console.log('hello',data);} catch(err){
+    console.log(err)
+ }
+ 
 }
 submitHandler();
